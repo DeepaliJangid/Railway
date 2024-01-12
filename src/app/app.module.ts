423b0ff0963ec2f388cd206a3c050e6711f834ef
Deepali Jangid <deepali.jangid@cubastion.com>
2024-01-12 11:29:57 +0530

    I created a material.module.ts file and installed toastr using command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+// import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: "toast-top-right",
+      preventDuplicates: true,
+      progressBar:true,
+      closeButton:true,
+      tapToDismiss:true,
+      progressAnimation:'increasing',
+      newestOnTop:true,
+      toastClass:'toastr',
+    }),
+  
+    
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
